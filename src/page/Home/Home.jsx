bcfebce0620d690add887bd009ed77689b4d46db
pc <www.mmhmdmm83@gmail.com>
2022-07-14 15:43:54 +0430

    responsive Home Section

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="home w-full flex">
+    <div className="home w-full flex p-x">
       <div className="avatar-container flex center">
         <div className="avatar-image-box">
           <img className="avatar-image" src="image/me.jpg" alt="" />
@@ -12,9 +12,7 @@ function Home() {
       <div className="myName-container flex center">
         <span className="hello-text">Hello, I'm</span>
         <span className="name-text">Mohammad Yousefvand</span>
-        <span className="job-text">
-          Front End Developer
-        </span>
+        <span className="job-text">Front End Developer</span>
         <button className="download-Cv">Download CV</button>
       </div>
     </div>
