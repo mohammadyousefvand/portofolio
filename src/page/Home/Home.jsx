86f82fc64cb5b4ce726668e40211860941291df8
pc <www.mmhmdmm83@gmail.com>
2022-07-14 15:26:55 +0430

    Home: work and style in name section

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -4,12 +4,19 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home w-full flex">
-      <div className="avatar flex center">
-        <div className="image-box">
+      <div className="avatar-container flex center">
+        <div className="avatar-image-box">
           <img className="avatar-image" src="image/me.jpg" alt="" />
         </div>
       </div>
-      <div className="myName flex center">hasan</div>
+      <div className="myName-container flex center">
+        <span className="hello-text">Hello, I'm</span>
+        <span className="name-text">Mohammad Yousefvand</span>
+        <span className="job-text">
+          Front End Developer
+        </span>
+        <button className="download-Cv">Download CV</button>
+      </div>
     </div>
   );
 }
