aa528ea04026729ecd9ef63092ae81ac3139b286
pc <www.mmhmdmm83@gmail.com>
2022-07-19 18:25:38 +0430

    Skill: complate Base in Skill Page

--- src/page/Skills/Skills.jsx
+++ src/page/Skills/Skills.jsx
@@ -4,20 +4,37 @@ import "./Skills.css";
 import ProgressBar from "../../Components/ProgressBar/ProgressBar";
 
 function Skills() {
-  const skillsRef = useRef([
+  const firstskillsRef = useRef([
     { title: "HTML", value: 80 },
     { title: "CSS", value: 60 },
     { title: "Javascript", value: 60 },
     { title: "React", value: 70 }
   ]);
 
+  const secondskillsRef = useRef([
+    "Bootstrap",
+    "Tailwind",
+    "Styled-component",
+    "Redux",
+    "Sass",
+    "Git/Github",
+    "Material UI"
+  ]);
+
   return (
     <div className="skills w-full flex p-x">
       <div className="skill-Box flex">
-        {skillsRef.current.map((skill, index) => (
-          <div className="skill-item">
-            <SkillBox key={index} title={skill.title} value={skill.value} />
+        {firstskillsRef.current.map((skill, index) => (
+          <div key={index} className="skill-item">
+            <SkillBox title={skill.title} value={skill.value} />
             <ProgressBar value={skill.value} title={skill.title} />
+          </div>
+        ))}
+      </div>
+      <div className="skill-Box flex">
+        {secondskillsRef.current.map((skill, index) => (
+          <div key={index} className="second-skill-item">
+            <SkillBox title={skill} />
           </div>
         ))}
       </div>
