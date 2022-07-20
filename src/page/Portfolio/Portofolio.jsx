1a0fb6e5350d7422fc065a5b3d380e823cf2e362
pc <www.mmhmdmm83@gmail.com>
2022-07-20 17:01:01 +0430

    Portofolio: fix and responsive Page

--- src/page/Portfolio/Portofolio.jsx
+++ src/page/Portfolio/Portofolio.jsx
@@ -23,6 +23,38 @@ function Portofolio() {
             <Button content="Live demo" width={160} />
           </div>
         </div>
+        <div className="project-item">
+          <div className="project-item-coverBox">
+            <img
+              className="project-item-cover"
+              src="image/Screenshot from 2022-07-01 17-13-40.png"
+              alt=""
+            />
+          </div>
+          <div className="project-description">
+            <span>Lorem ipsum dolor sit amet.</span>
+          </div>
+          <div className="project-item-btnBox flex">
+            <Button content="Github" width={100} />
+            <Button content="Live demo" width={160} />
+          </div>
+        </div>
+        <div className="project-item">
+          <div className="project-item-coverBox">
+            <img
+              className="project-item-cover"
+              src="image/Screenshot from 2022-07-01 17-13-40.png"
+              alt=""
+            />
+          </div>
+          <div className="project-description">
+            <span>Lorem ipsum dolor sit amet.</span>
+          </div>
+          <div className="project-item-btnBox flex">
+            <Button content="Github" width={100} />
+            <Button content="Live demo" width={160} />
+          </div>
+        </div>
       </div>
     </div>
   );
