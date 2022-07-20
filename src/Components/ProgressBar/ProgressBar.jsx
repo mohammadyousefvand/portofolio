0c2821038905aa98d06e3a96e8f4171f8d10efa7
pc <www.mmhmdmm83@gmail.com>
2022-07-20 18:19:43 +0430

    ProgressBar: Add Scroll efect to progress Bar

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -2,34 +2,41 @@ import React from "react";
 import "./ProgressBar.css";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import { InView } from "react-intersection-observer";
 
 function ProgressBar({ value, title }) {
   return (
-    <div className="progress-skills">
-      <CircularProgressbar
-        value={value}
-        text={`${value}%`}
-        styles={buildStyles({
-          ...(title === "HTML" && {
-            pathColor: "#e34c26",
-            textColor: "#e34c26"
-          }),
-          ...(title === "CSS" && {
-            pathColor: "#264de4",
-            textColor: "#3f87e0"
-          }),
-          ...(title === "Javascript" && {
-            pathColor: "#f0db4f",
-            textColor: "#f0db4f"
-          }),
-          ...(title === "React" && {
-            pathColor: "#61DBFB",
-            textColor: "#61DBFB"
-          }),
-          trailColor: "#130952"
-        })}
-      />
-    </div>
+    <InView delay={500}>
+      {({ inView, ref }) => {
+        return (
+          <div className="progress-skills" ref={ref} delay={100}>
+            <CircularProgressbar
+              value={inView ? value : 0}
+              text={`${value}%`}
+              styles={buildStyles({
+                ...(title === "HTML" && {
+                  pathColor: "#e34c26",
+                  textColor: "#e34c26"
+                }),
+                ...(title === "CSS" && {
+                  pathColor: "#264de4",
+                  textColor: "#3f87e0"
+                }),
+                ...(title === "Javascript" && {
+                  pathColor: "#f0db4f",
+                  textColor: "#f0db4f"
+                }),
+                ...(title === "React" && {
+                  pathColor: "#61DBFB",
+                  textColor: "#61DBFB"
+                }),
+                trailColor: "#130952"
+              })}
+            />
+          </div>
+        );
+      }}
+    </InView>
   );
 }
 
