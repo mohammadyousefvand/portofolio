6b9aa63d294ecf44b5f9acbe6d955a6c4af5a246
pc <www.mmhmdmm83@gmail.com>
2022-07-20 16:26:54 +0430

    create New page : Portofolio

---
+++ src/page/Portfolio/Portofolio.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Portfolio.css";
+
+function Portofolio() {
+  return (
+    <div className="portfolio flex w-full p-x">
+      <span className="portfolio-title">My Portofolio</span>
+    </div>
+  );
+}
+
+export default Portofolio;
