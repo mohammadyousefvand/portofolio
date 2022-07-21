3f61631413971feed32d35f9d1a74f88fa5e14bc
pc <www.mmhmdmm83@gmail.com>
2022-07-21 17:29:17 +0430

    create ProjectBox Component

--- src/page/Portfolio/Portofolio.jsx
+++ src/page/Portfolio/Portofolio.jsx
@@ -1,60 +1,15 @@
 import React from "react";
-import Button from "../../Components/Button/Button";
 import "./Portofolio.css";
+import ProjectBox from "./ProjectBox/ProjectBox";
 
 function Portofolio() {
   return (
     <div className="portfolio flex w-full p-x">
       <span className="portfolio-title">My Portofolio</span>
       <div className="project-wrapper">
-        <div className="project-item">
-          <div className="project-item-coverBox">
-            <img
-              className="project-item-cover"
-              src="image/Screenshot from 2022-07-01 17-13-40.png"
-              alt=""
-            />
-          </div>
-          <div className="project-description">
-            <span>Lorem ipsum dolor sit amet.</span>
-          </div>
-          <div className="project-item-btnBox flex">
-            <Button content="Github" width={100} />
-            <Button content="Live demo" width={160} />
-          </div>
-        </div>
-        <div className="project-item">
-          <div className="project-item-coverBox">
-            <img
-              className="project-item-cover"
-              src="image/Screenshot from 2022-07-01 17-13-40.png"
-              alt=""
-            />
-          </div>
-          <div className="project-description">
-            <span>Lorem ipsum dolor sit amet.</span>
-          </div>
-          <div className="project-item-btnBox flex">
-            <Button content="Github" width={100} />
-            <Button content="Live demo" width={160} />
-          </div>
-        </div>
-        <div className="project-item">
-          <div className="project-item-coverBox">
-            <img
-              className="project-item-cover"
-              src="image/Screenshot from 2022-07-01 17-13-40.png"
-              alt=""
-            />
-          </div>
-          <div className="project-description">
-            <span>Lorem ipsum dolor sit amet.</span>
-          </div>
-          <div className="project-item-btnBox flex">
-            <Button content="Github" width={100} />
-            <Button content="Live demo" width={160} />
-          </div>
-        </div>
+        <ProjectBox />
+        <ProjectBox />
+        <ProjectBox />
       </div>
     </div>
   );
---
+++ src/page/Portfolio/ProjectBox/ProjectBox.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./ProjectBox.css";
+import Button from "../../../Components/Button/Button";
+
+function ProjectBox() {
+  return (
+    <div className="project-item">
+      <div className="project-item-coverBox">
+        <img
+          className="project-item-cover"
+          src="image/Screenshot from 2022-07-01 17-13-40.png"
+          alt=""
+        />
+      </div>
+      <div className="project-description">
+        <span>Lorem ipsum dolor sit amet.</span>
+      </div>
+      <div className="project-item-btnBox flex">
+        <Button content="Github" width={100} />
+        <Button content="Live demo" width={160} />
+      </div>
+    </div>
+  );
+}
+
+export default ProjectBox;
