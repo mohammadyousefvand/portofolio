d5542a24778d0033e1a0a0821710911311e982be
pc <www.mmhmdmm83@gmail.com>
2022-07-21 17:47:43 +0430

    create NavBar Component

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="navBar">
+      <span>...</span>
+    </div>
+  );
+}
+
+export default NavBar;
