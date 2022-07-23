434c643543a7827cc249b07fe3d2a63e310ccf00
pc <www.mmhmdmm83@gmail.com>
2022-07-23 16:39:56 +0430

    Error: Add Error icon to err message

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Error.css";
+import { MdError } from "react-icons/md";
 
 function Error({ children }) {
-  return <div className="err">{children}</div>;
+  return (
+    <div className="err-wrapper flex">
+      <MdError className="err-icon" />
+      <span className="err-text">{children}</span>
+    </div>
+  );
 }
 
 export default Error;
