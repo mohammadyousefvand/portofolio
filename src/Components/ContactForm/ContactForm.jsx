e14bd4822bd08523e3fa894889809fb47c44eaea
pc <www.mmhmdmm83@gmail.com>
2022-07-23 10:32:55 +0430

    Form: change Form Component Name To ContactForm

---
+++ src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ContactForm.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+function ContactForm() {
+  return (
+    <div className="form">
+      <span>...</span>
+    </div>
+  );
+}
+
+export default ContactForm;
