09183ed611dd3262fefe817298cbc1d680e7f5f1
pc <www.mmhmdmm83@gmail.com>
2022-07-23 10:14:23 +0430

    create Contact page

---
+++ src/page/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact() {
+  return (
+    <div className='contact flex w-full p-x'>
+        
+    </div>
+  )
+}
+
+export default Contact
