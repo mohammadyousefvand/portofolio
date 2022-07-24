a8e5586cfb633fe09a7453cbd20098be9259cfa7
pc <www.mmhmdmm83@gmail.com>
2022-07-24 11:35:03 +0430

    ContactInfoBox: Responsive Section

--- src/Components/ContactInfoBox/ContactInfoBox.jsx
+++ src/Components/ContactInfoBox/ContactInfoBox.jsx
@@ -10,25 +10,25 @@ function ContactInfoBox() {
       <div className="info-box-wrapper flex">
         <FaTelegramPlane className="info-icon" />
         <span>Telegram</span>
-        <span>+98 9394094563</span>
+        <span className="info-user-number">+98 9394094563</span>
         <span className="info-send">send a message</span>
       </div>
       <div className="info-box-wrapper flex">
         <BsWhatsapp className="info-icon" />
         <span>Whatsapp</span>
-        <span>+98 9394094563</span>
+        <span className="info-user-number">+98 9394094563</span>
         <span className="info-send">send a message</span>
       </div>
       <div className="info-box-wrapper flex">
         <MdOutlineMailOutline className="info-icon" />
         <span>Email</span>
-        <span>mohammad@gmail.com</span>
+        <span className="info-user-number">mohammad@gmail.com</span>
         <span className="info-send">send a message</span>
       </div>
       <div className="info-box-wrapper flex">
         <FaLinkedinIn className="info-icon" />
         <span>Linkedin</span>
-        <span>mohammadyousefvand</span>
+        <span className="info-user-number">mohammadyousefvand</span>
         <span className="info-send">send a message</span>
       </div>
     </>
