45c473557cc63b7aaef8783d48970a88e5c80696
pc <www.mmhmdmm83@gmail.com>
2022-07-25 11:45:30 +0430

    ContactInfoBox: Add send message link to Component

--- src/Components/ContactInfoBox/ContactInfoBox.jsx
+++ src/Components/ContactInfoBox/ContactInfoBox.jsx
@@ -11,25 +11,39 @@ function ContactInfoBox() {
         <FaTelegramPlane className="info-icon" />
         <span>Telegram</span>
         <span className="info-user-number">+98 9394094563</span>
-        <span className="info-send">send a message</span>
+        <a href="https://t.me/fcb_6666" className="info-send">
+          Send a Message
+        </a>
       </div>
       <div className="info-box-wrapper flex">
         <BsWhatsapp className="info-icon" />
         <span>Whatsapp</span>
         <span className="info-user-number">+98 9394094563</span>
-        <span className="info-send">send a message</span>
+        <a href="https://wa.me/989394094563" className="info-send">
+          Send a Message
+        </a>
       </div>
       <div className="info-box-wrapper flex">
         <MdOutlineMailOutline className="info-icon" />
         <span>Email</span>
         <span className="info-user-number">mohammad@gmail.com</span>
-        <span className="info-send">send a message</span>
+        <a
+          href="https://mohammadyousefvand1999@gmail.com/"
+          className="info-send"
+        >
+          Send a Message
+        </a>
       </div>
       <div className="info-box-wrapper flex">
         <FaLinkedinIn className="info-icon" />
         <span>Linkedin</span>
         <span className="info-user-number">mohammadyousefvand</span>
-        <span className="info-send">send a message</span>
+        <a
+          href="https://www.linkedin.com/in/mohammad-yousefvand-a9b045226/"
+          className="info-send"
+        >
+          See Profile
+        </a>
       </div>
     </>
   );
