f71145a4a534c5310744456084b769ccd2a3e63a
pc <www.mmhmdmm83@gmail.com>
2022-07-27 17:23:39 +0430

    create SocialMedia Component

---
+++ src/Components/SocialMedia/SocialMedia.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SocialMedia.css";
+
+function SocialMedia() {
+  return (
+    <div>
+      <span>...</span>
+    </div>
+  );
+}
+
+export default SocialMedia;
