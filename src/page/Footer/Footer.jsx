0238683190ab825eebc129c352e75611cc1cdd62
pc <www.mmhmdmm83@gmail.com>
2022-07-30 15:27:22 +0430

    Footer: add link to social icons

--- src/page/Footer/Footer.jsx
+++ src/page/Footer/Footer.jsx
@@ -7,16 +7,22 @@ import { FiGitlab } from "react-icons/fi";
 function Footer() {
   return (
     <div className="footer flex">
-      <a href="http://localhost:3000/" className="footer-link">
+      <a href="https://github.com/mohammadyousefvand" className="footer-link">
         <GoMarkGithub className="footer-icon" />
       </a>
-      <a href="http://localhost:3000/" className="footer-link">
+      <a
+        href="https://www.instagram.com/mohammad_yousefvand_/"
+        className="footer-link"
+      >
         <BsInstagram className="footer-icon" />
       </a>
-      <a href="http://localhost:3000/" className="footer-link">
+      <a
+        href="https://www.linkedin.com/in/mohammad-yousefvand-a9b045226/"
+        className="footer-link"
+      >
         <BsLinkedin className="footer-icon" />
       </a>
-      <a href="http://localhost:3000/" className="footer-link">
+      <a href="https://gitlab.com/mohammadyousefvand" className="footer-link">
         <FiGitlab className="footer-icon" />
       </a>
     </div>
